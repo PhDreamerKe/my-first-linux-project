3fa46cf9d7fb91d25bb2cd836d4caa3047c815ed
PhDreamerKe <itsmburuisaac17@gmail.com>
2026-09-19 03:10:58 +0300

    second commit
    Add usage instructions for running the backup script.

---
+++ runscript.sh
@@ -0,0 +1,9 @@
+2. Running the Script
+From the my-bash-backup directory, run:
+
+./backup_script.sh
+You should see output like:
+
+Backup created: ./backups/test_data_backup_20241201_123456.zip
+Check the backups directory for your zip file.
+
