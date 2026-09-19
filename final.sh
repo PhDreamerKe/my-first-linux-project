dc56efb41e23d923c4a07d559a895f90cc44f9b3
PhDreamerKe <itsmburuisaac17@gmail.com>
2026-09-19 03:13:39 +0300

    fourth commit
    Added sections for dependencies and troubleshooting.

---
+++ final.sh
@@ -0,0 +1,9 @@
+6. Dependencies
+Make sure you have zip installed:
+
+sudo apt install zip   # On Ubuntu/Debian
+sudo dnf install zip   # On Fedora
+
+7. Troubleshooting
+If you get a "permission denied" error, make sure the script is executable (chmod +x backup_script.sh).
+If you see "zip command not found," install zip as above.
