ee58426ac393255ca3d2dc213e295bc96a1261af
PhDreamerKe <itsmburuisaac17@gmail.com>
2026-09-19 03:12:12 +0300

    third commit
    Added documentation on customization, learning outcomes, and next steps.

---
+++ threesteps.sh
@@ -0,0 +1,17 @@
+3. Customization
+You can change:
+
+The SOURCE_DIR to back up a different folder.
+The BACKUP_DIR to store backups elsewhere.
+
+4. What You Learned
+How to write and run a Bash script.
+How to use variables and user input.
+How to generate timestamps.
+How to handle basic error checking.
+How to use the zip command.
+
+5. Next Steps
+Try adding more files to test_data and running the backup again.
+Modify the script to email the backup file.
+Add a command-line argument so you can specify the source directory.
